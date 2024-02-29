6a2d69d672d70fbcafd5781568b6e04545cdec69
Arquimedes15 <143116307+Arquimedes15@users.noreply.github.com>
2024-02-29 01:43:42 -0500

    Add files via upload

---
+++ _astro/slider.A1RCp1ng.js
@@ -0,0 +1,1 @@
+import{j as s}from"./jsx-runtime.K1e75nIr.js";import"./index.NEDEFKed.js";function r(){return s.jsxs("div",{id:"slider_1",children:[s.jsx("div",{class:"slider"}),s.jsxs("div",{class:"buttons",children:[s.jsx("button",{class:"prev",children:"Anterior"}),s.jsx("button",{class:"next",children:"Siguiente"})]}),s.jsx("div",{class:"indicator-buttons"})]})}export{r as default};
