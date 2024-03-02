51055da45b0690624293f211f7b859d8658b92bf
Arquimedes15 <143116307+Arquimedes15@users.noreply.github.com>
2024-03-02 00:31:30 -0500

    Add files via upload

---
+++ _astro/hoisted.C4-qDx61.js
@@ -0,0 +1,1 @@
+let t=document.querySelector("#cerr-menu-a"),e=document.querySelector("#bar-menu-android"),r=document.querySelector(".bars");t.addEventListener("click",()=>{e.style.display="none"});r.addEventListener("click",()=>{e.style.display="block"});
---
+++ _astro/hoisted.DwGB2sVE.js
@@ -0,0 +1,1 @@
+import"https://arquimedes15.github.io/297-Jardin-Piloto/scripts/slider.js";import"./hoisted.C4-qDx61.js";
