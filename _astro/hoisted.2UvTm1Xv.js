159c220a9310a8a9f1b71d24b25f2bd79d1d0c23
Arquimedes15 <143116307+Arquimedes15@users.noreply.github.com>
2024-03-02 00:56:56 -0500

    Add files via upload

---
+++ _astro/hoisted.2UvTm1Xv.js
@@ -0,0 +1,21 @@
+import"./hoisted.CkeWFOi-.js";import"./hoisted.C4-qDx61.js";let s="",i=document.querySelector(".content-news");for(let e=0;e<10;e++)s+=`
+                    <div class="news-cont">
+                        <h4>Comunicado N°${e}</h4>
+                        <hr>
+                        <div class="content">
+                            <div class="fech">
+                                <p class="mes">Feb</p>
+                                <p class="day">14</p>
+                                <p class="year">2024</p>
+                            </div>
+                            <p class="content-text">
+                                Hola como estan<br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod
+                                libero
+                                doloremque aspernatur eligendi vero sit aliquid ipsa? Fugiat, quae quam molestiae
+                                dolorum
+                                accusamus impedit voluptas, asperiores, quasi rem iusto itaque?
+                            </p>
+                            <img src="https://avatarfiles.alphacoders.com/119/thumb-1920-119537.jpg" alt="Noticias"
+                                class="img-news">
+                        </div>
+                    </div>`;i.innerHTML=s;
