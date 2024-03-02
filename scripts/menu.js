61236e83be3d9a8de1d3e513288d2fe14f645e26
Arquimedes15 <143116307+Arquimedes15@users.noreply.github.com>
2024-03-02 00:55:51 -0500

    Add files via upload

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,12 +1,11 @@
-document.addEventListener('load', () => {
-    let cerr_ma = document.querySelector("#cerr-menu-a")
-    let menuA_a = document.querySelector("#bar-menu-android");
-    let most_menuaA = document.querySelector(".bars")
 
-    cerr_ma.addEventListener("click", () => {
-        menuA_a.style.display = "none"
-    })
-    most_menuaA.addEventListener("click", () => {
-        menuA_a.style.display = "block"
-    })
+let cerr_ma = document.querySelector("#cerr-menu-a")
+let menuA_a = document.querySelector("#bar-menu-android");
+let most_menuaA = document.querySelector(".bars")
+
+cerr_ma.addEventListener("click", () => {
+    menuA_a.style.display = "none"
+})
+most_menuaA.addEventListener("click", () => {
+    menuA_a.style.display = "block"
 })
